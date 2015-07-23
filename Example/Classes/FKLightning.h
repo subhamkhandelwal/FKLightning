acf66be07e65ff2c5c06785e2f8ef73d8256386f
Subham Khandelwal <subham.khandelwal@flipkarts-MacBook-Pro-47.local>
2015-07-23 15:46:13 +0530

    test file

---
+++ Example/Classes/FKLightning.h
@@ -0,0 +1,16 @@
+//
+//  FKLightning.h
+//  FKLightning
+//
+//  Created by subham khandelwal on 23/07/15.
+//  Copyright (c) 2015 Subham Khandelwal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FKLightning : NSObject
+
+@property (strong, nonatomic) NSString *testString;
+- (void) printString;
+
+@end
---
+++ Example/Classes/FKLightning.m
@@ -0,0 +1,18 @@
+//
+//  FKLightning.m
+//  FKLightning
+//
+//  Created by subham khandelwal on 23/07/15.
+//  Copyright (c) 2015 Subham Khandelwal. All rights reserved.
+//
+
+#import "FKLightning.h"
+
+@implementation FKLightning
+
+- (void) printString
+{
+    NSLog(@"Str: %@", _testString);
+}
+
+@end
